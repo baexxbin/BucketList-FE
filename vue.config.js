be1d67ac012e10c81e8ebc6abf828d234ae9bb15
baexxbin <soo3886@gmail.com>
2023-06-10 19:41:19 +0900

    feat: BucketList-FE(spring)과 연동

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  // npm run build 타겟 디렉토리(백엔드 부분: spring-boot 프로젝트의 resource/static 경로)
+  outputDir: "../bucketlist/src/main/resources/static",
+
+  /**
+   * npm run server 개발 진행시 포트가 다르므로 프록시 설정
+   * target: 백엔드 port
+   * changeOrigin : true >> cros 문제 해결
+   */
+  devServer: {
+    proxy: {
+      '/api': {  // main페이지 열릴 때 연결되는 경로
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      }
+    }
+  }
 })
