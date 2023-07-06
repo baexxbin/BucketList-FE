7d49750a5e9c6b5ffaca58c65911f4715e57ce25
baexxbin <soo3886@gmail.com>
2023-07-06 13:32:01 +0900

    fix: 불필요한 pagination 호출 수정

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -4,23 +4,21 @@
       <HeaderView />
     </div>
     <div>
-      <template>
-        <v-data-table :headers="tableHeaders" :items="fetchItems" :total-items="totalItems" :loading="loading" v-model:pagination="pagination" @pagination="onPagination">
-          <template v-slot:headers="{ headers }">
-            <tr>
-              <th v-for="header in headers" :key="header.text">{{ header.text }}</th>
-            </tr>
-          </template>
-          <template v-slot:body="{ items }">
-            <tr v-for="item in items" :key="item.registerId">
-              <td>{{ item.registerId }}</td>
-              <td>{{ item.name }}</td>
-              <td>{{ item.goal }}</td>
-              <td>{{ item.targetDate }}</td>
-            </tr>
-          </template>
-        </v-data-table>
-      </template>
+      <v-data-table :headers="tableHeaders" :items="fetchItems" :server-items-length="totalItems" :loading="loading" @pagination="onPagination">
+        <template v-slot:headers="{ headers }">
+          <tr>
+            <th v-for="header in headers" :key="header.text">{{ header.text }}</th>
+          </tr>
+        </template>
+        <template v-slot:body="{ items }">
+          <tr v-for="item in items" :key="item.registerId">
+            <td>{{ item.registerId }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.goal }}</td>
+            <td>{{ item.targetDate }}</td>
+          </tr>
+        </template>
+      </v-data-table>
     </div>
     <div class="footer">
       <FooterView />
@@ -29,72 +27,65 @@
 </template>
 
 <script>
-import { defineComponent, ref, onMounted } from 'vue';
 import HeaderView from '@/views/layout/HeaderView.vue';
 import FooterView from '@/views/layout/FooterView.vue';
 import axios from 'axios';
 
-export default defineComponent({
+export default {
   components: {
     HeaderView,
     FooterView,
   },
-  setup() {
-    const tableHeaders = [
-      { text: 'Register ID', value: 'registerId' },
-      { text: '이름', value: 'name' },
-      { text: '목표', value: 'goal' },
-      { text: '목표일', value: 'targetDate' },
-    ];
-
-    const fetchItems = ref([]);
-    const totalItems = ref(0);
-    const loading = ref(false);
-    const pagination = ref({
-      page: 1,
-      itemsPerPage: 10,
-    });
-
-    const fetchData = () => {
-      loading.value = true;
+  data() {
+    return {
+      tableHeaders: [
+        { text: 'Register ID', value: 'registerId' },
+        { text: '이름', value: 'name' },
+        { text: '목표', value: 'goal' },
+        { text: '목표일', value: 'targetDate' },
+      ],
+      fetchItems: [],
+      totalItems: 0,
+      loading: false,
+      pagination: {
+        page: 1,
+        itemsPerPage: 10,
+      },
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.loading = true;
       axios
         .get('/api/v3/simple-orders', {
           params: {
-            page: pagination.value.page,
-            limit: pagination.value.itemsPerPage,
+            page: this.pagination.page,
+            limit: this.pagination.itemsPerPage,
           },
         })
         .then(response => {
-          fetchItems.value = response.data;
-          totalItems.value = response.data.length;
+          this.fetchItems = response.data;
+          this.totalItems = response.headers['x-total-count'];
         })
         .catch(error => {
           console.error(error);
         })
         .finally(() => {
-          loading.value = false;
+          this.loading = false;
         });
-    };
-
-    const onPagination = (pagination) => {
-      pagination.value = pagination;
-      fetchData();
-    };
-
-    onMounted(fetchData);
-
-    return {
-      tableHeaders,
-      fetchItems,
-      totalItems,
-      loading,
-      pagination,
-      onPagination,
-    };
+    },
+    onPagination() {
+      if (this.pagination.page !== this.pagination.prevPage) {
+        this.fetchData();
+      }
+    },
   },
-});
+};
 </script>
 
 <style>
-/* 사용자 정의 스타일 */
+/* 사용자정의 스타일 */
 </style>
